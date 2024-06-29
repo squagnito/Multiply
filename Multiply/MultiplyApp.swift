aab6fb744bc402a4184fc58c985d28c3ef262154
Aaron Medina <squagnito@gmail.com>
2024-06-29 15:06:19 -0500

    Initial Commit

---
+++ Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Aaron Medina on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
