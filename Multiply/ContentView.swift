7da99229ecb912699cb31aacd52fd3d678b63c21
Aaron Medina <squagnito@gmail.com>
2024-07-01 19:16:43 -0500

    set up starting UI

--- Multiply/ContentView.swift
+++ Multiply/ContentView.swift
@@ -8,12 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tableSelection = 2
+    @State private var numQuestions = 5
+    
+    let numQuestionsOptions = [5, 10, 15, 20]
+    let tables = Array(2...15)
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
+        Text("Multiply")
+            .padding(.vertical, 30)
+            .frame(maxWidth: .infinity)
+            .font(.title)
+            .fontWeight(.bold)
+            .background(.cyan)
+            .foregroundStyle(.white)
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Which table do you want to practice?")
+                .padding(.vertical, 30)
+                .font(.headline)
+            
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(2..<21) { number in
+                    Button(action: {
+                        tableSelection = number
+                    }, label: {
+                        Text("\(number)")
+                            .fontWeight(tableSelection == number ? .bold : .semibold)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(tableSelection == number ? .purple : .orange)
+                            .foregroundStyle(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                    })
+                }
+            }
+            
+            Spacer()
+            
+            Text("How many problems do you want to practice?")
+                .padding(.bottom, 15)
+                .font(.headline)
+            
+            Picker("How many problems do you want to practice?", selection: $numQuestions) {
+                ForEach(numQuestionsOptions, id: \.self) { option in
+                    Text("\(option)")
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            Spacer()
+            Button(action: {
+                // Start game
+            }, label: {
+                HStack {
+                    Text("START")
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(.white)
+                        .background(.purple)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            })
         }
         .padding()
     }
